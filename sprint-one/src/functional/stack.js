da8bbad069c8427894124e01c12c83ad83bca0ab
Quin Kinser <QuinKinser@gmail.com>
2016-06-01 16:44:07 -0700

    Implement stack data structure (functional pattern)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,8 +1,32 @@
+  // Last in First Out (like a stack of cafeteria trays)
+  // Can only add to top and remove from top
+
+  // requirements:
+  // Create a size method/function.
+  // Create a push method/function.
+  // Create a pop method/function.
 var Stack = function(){
- 
-
-  // Use an object with numeric keys to store values
   
-
-  // return instance of stack;
+  var newStack = {
+    length: 0,
+    size: function() {
+    	return this.length;
+    }, 
+    push: function(element) {
+      this[this.length] = element;
+      this.length += 1;
+    },
+    pop: function() {
+      if (this.length === 0) {
+        return "NO";
+      }
+      var returnVal = this[this.length - 1];
+      delete this[this.length - 1];
+      this.length -= 1;
+      return returnVal;
+    } 
+  };
+  
+  return newStack;
+  
 };
