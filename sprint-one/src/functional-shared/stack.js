9dc3e77f1f99a92a21d85535ee34c5741234b9f0
Justin Ross <Ross.Justin.T@gmail.com>
2016-06-01 17:27:48 -0700

    Build Stack.js for functional-shared data-structure - pass all tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,3 +1,37 @@
 var Stack = function() {
-  
+
+  var stackMethods = {
+  	push: function(elem) {
+  		this[this.length] = elem;
+
+  		this.length++;
+  	},
+  	pop: function() {
+
+  		if(this.length === 0) {
+  			return "negative brother";
+  		}
+
+  		var returnVal = this[this.length - 1];
+  		delete this[this.length - 1];
+
+  		this.length--;
+
+  		return returnVal;
+
+  	},
+  	size: function() {
+  		return this.length;
+  	}
+  };
+
+  var newStack = {
+  	length: 0,
+  	push: stackMethods.push,
+  	pop: stackMethods.pop,
+  	size: stackMethods.size
+  };
+
+
+  return newStack;
 };
